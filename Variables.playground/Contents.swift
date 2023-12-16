bf28f5b77148abffd57194b03c6b89066415050e
Yusuf Arfan Ismail <yusufgamer222@gmail.com>
2023-12-16 21:57:27 +0000

    Finished with Variables

--- Variables.playground/Contents.swift
+++ Variables.playground/Contents.swift
@@ -1,3 +1,49 @@
-import UIKit
+import Foundation
 
-var greeting = "Hello, playground"
+// two different syntax is var and let
+
+// the difference between let and var is that let can not be assigned to again
+
+let myName = "Yusuf"
+let yourName = "Foo"
+
+// if you use let for a list you can edit it.
+var names = [myName, yourName]
+
+names.append("Bar")
+
+
+let foo = "foo"
+
+var foo2 = foo
+
+foo2 = "foo 2"
+
+let moreNames : [String] = ["Foo", "Bar"]
+
+var copy = moreNames
+
+copy.append("Baz")
+
+moreNames
+copy
+
+let someNames = NSMutableArray(
+    array: [
+        "Foo",
+        "Bar"
+    ]
+)
+
+func changeTheArray(_ array : NSArray) {
+    
+    // does not copy the array but just assigning it. BAD CODE
+    let copy = array as! NSMutableArray
+    
+    copy.add("Baz")
+}
+
+changeTheArray(someNames)
+
+// Been altered.
+someNames
